584287f7bec5f3fb8ba48f81576eebaa3c75ac19
mai cook <mailiecook@yahoo.com>
2020-06-25 14:07:13 -0700

    full functionality to test haiku for proper amount of syllables and lines

--- src/Haiku.js
+++ src/Haiku.js
@@ -6,20 +6,13 @@ const storeState = (poem) => {
     return newState;
   };
 };
-
-const syllablesCount = (line) => {
+const syllablesCount = (line, syllables = 0) => {
   const vowelArray = ["a", "e", "i", "o", "u"];
-  let wordArray = line.split(" "); //split on spaces
-  console.log(wordArray);
-  let syllables = 0;
+  let wordArray = line.toLowerCase().split(" ");
   wordArray.forEach(word => {
     let charArray = word.split('');
-    console.log(charArray);
-    // charArray.forEach(char => {
       for(let i = 0; i < charArray.length; i++){
-        console.log(charArray[i]);
         let char = charArray[i];
-        //y is a vowel & constitutes a new syllable if and only if it is surrounded by consonants or at the end
         if (char == "y"){
           if (charArray.length == i +1) {
             if (vowelArray.includes(charArray[i-1])) {
@@ -59,24 +52,14 @@ const syllablesCount = (line) => {
         }  
       }
   });   
-  console.log(syllables);
   return syllables;    
 };
 
-const isHaiku = (line1, line2, line3, haikuBool = true) => {
-  //for each line, run syllablesCount
-  //for line1, if !(syllables = 5) {
-  // return haikuBool = false;
-  // }
-
-  //line 1 - 5 syllables, line 2 - 7 syllables, line 3 - 5 
-  while (haikuBool == true) {
-    // const line1Syllables = syllablesCount(line1);
-    if (line1Syllables == 5) {
-      // const line2Syllables = syllablesCount(line2);
-      if (line2Syllables == 7) {
-        // const line3Syllables = syllablesCount(line3);
-        if (line3Syllables == 5) {
+const isHaiku = (poem, haikuBool = true) => {
+  while (haikuBool == true) { 
+    if (syllablesCount(poem.line1) == 5) {
+      if (syllablesCount(poem.line2) == 7) {
+        if (syllablesCount(poem.line3) == 5) {
           return haikuBool = true;
         }
       }
@@ -85,15 +68,7 @@ const isHaiku = (line1, line2, line3, haikuBool = true) => {
       return haikuBool = false;
     }
   }
-}
-
-
-//last in first out
-//in change state we took in the prop then we returned the value added on and then lastly state(obj gets made)
-
-
-
-
+};
 $(document).ready(function () {
   $("form").submit(function (event) {
     event.preventDefault();
@@ -107,16 +82,7 @@ $(document).ready(function () {
     $("#line1-value").text(poem.line1);
     $("#line2-value").text(poem.line2);
     $("#line3-value").text(poem.line3);
-    const syllables1 = (syllablesCount(poem.line1));
-    const syllables2 = (syllablesCount(poem.line2));
-    const syllables3 = (syllablesCount(poem.line3));
-    const confirm = (isHaiku(syllables1, syllables2, syllables3));
-    console.log(poem.line1);
-    console.log(poem.line2);
-    console.log(poem.line3);
-  
-    console.log(confirm + "  "+syllables1 + " "+ syllables2 + " " + syllables3);
+    const confirm = (isHaiku(poem));
+    $("#confirm-value").text(confirm);
   });
-  // $("#haiku").click(function () {
-  // });
 });
